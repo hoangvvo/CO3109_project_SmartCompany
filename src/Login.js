ef4bbf69c1fcd8957b1d90ea93cd93d52af3b202
ToChauHaoNhan <tochauhaonhan0812@gmail.com>
2023-10-28 17:25:08 +0700

    Add Buttons

--- src/Login.js
+++ src/Login.js
@@ -11,51 +11,6 @@ class Login extends Component {
             password: '',
             correctAuth: false
         };
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleUsernameChange(event) {
-        const existingError = document.querySelector('.error-message');
-        if (existingError) {
-            existingError.remove();
-        }
-        this.setState({
-            username: event.target.value
-        });
-    }
-    handlePasswordChange(event) {
-        const existingError = document.querySelector('.error-message');
-        if (existingError) {
-            existingError.remove();
-        }
-        this.setState({
-            password: event.target.value
-        });
-    }
-    handleClick() {
-        const { username, password } = this.state;
-        
-        if (username === 'Abc' && password === 'Xyz') {
-        // Valid credentials - handle the login logic here
-            alert('Login successful');
-        } else {
-        // Invalid credentials - display an error message
-            const errorElement = document.createElement('h3');
-            errorElement.textContent = 'Wrong username/password';
-            errorElement.className = 'error-message';
-
-        // Find the inputBox div and append the error message to it
-            const inputBox = document.querySelector('.password');
-            inputBox.appendChild(errorElement);
-        }
-        console.log(this.state.username);
-        console.log(this.state.password);
-    }
-    togglePasswordVisibility() {
-        this.setState(prevState => ({
-            showPassword: !prevState.showPassword
-        }));
     }
     render() {
     return (
@@ -67,7 +22,6 @@ class Login extends Component {
                     id='name' 
                     className='inputField' 
                     placeholder='USERNAME' 
-                    onChange={this.handleUsernameChange}
                 />
             </div>
             <div className='inputBox password'>
@@ -76,15 +30,14 @@ class Login extends Component {
                     id='password'
                     className='inputField'
                     placeholder='PASSWORD'
-                    onChange={this.handlePasswordChange}
                 />
             </div>
             <div>
-                <input type="checkbox" onChange={() => this.togglePasswordVisibility()} className='checkPassword'/>
+                <input type="checkbox" className='checkPassword'/>
                 <label className="togglePassword">Show Password</label>
             </div>
-            <button type='button' className='button' onClick={this.handleClick}>SUBMIT</button>
-            <button type='button' className='button Google' onClick={this.handleClick}>Log in with Google</button>
+            <button type='button' className='button' >SUBMIT</button>
+            <button type='button' className='button Google'>Log in with Google</button>
         </div>
         );
     }
